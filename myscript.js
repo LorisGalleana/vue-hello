745b8e75f3ca4ca4a4f1cc9713ca6283fa5ff0be
LorisGalleana <loris.galleana@gmail.com>
2024-06-25 09:42:13 +0200

    feat add image

--- myscript.js
+++ myscript.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Hello Vue! Hello World!"
+            message: "Hello Vue! Hello World!",
+            image: "https://picsum.photos/id/1074/5000/3333"
         }
     }
 }).mount("#container");
